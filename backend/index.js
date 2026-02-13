7d80a6c3dbe8dcaf2a9cb54310dd1acb96ee6203
unknown <nassimaimadtahiri@gmail.com>
2026-02-13 21:12:10 +0100

    First Commit

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express=require("express")
+const mongoose=require("mongoose")
+const cors=require("cors")
+require("dotenv").config()
+const app=express()
+app.use(express.json())
+app.use(cors())
+const PORT=process.env.PORT || 5000
+const DATABASE=process.env.DATABASE
+mongoose.connect(DATABASE).then(()=>{
+    console.log(`Database connected`)
+}).catch(err=>{
+    console.log(err)
+})
+app.listen(PORT,()=>{
+    console.log(`App running on port ${PORT}`)
+})
